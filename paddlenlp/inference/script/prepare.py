471d08884610c84b315d8b03fb06d59c1bbc4d1e
xieyongliang <yongliangxie@hotmail.com>
2022-08-29 10:41:56 +0800

    cleanup

--- paddlenlp/inference/script/prepare.py
+++ paddlenlp/inference/script/prepare.py
@@ -1,12 +1,14 @@
 import sys
+import json
 import uuid
 
 path = sys.argv[1]
+field = sys.argv[2]
 file = open(path, 'r')
 lines = file.readlines()
 for line in lines:
-    pos = line.rfind('####')
-    text = line[0: pos]
+    data = json.loads(line)
+    text = data[field]
     tmp_file = open('{0}.txt'.format(str(uuid.uuid4())),'w')
     tmp_file.write(text)
     tmp_file.close()
